3c99c8a8db666702105c0a596f72bc42d6401105
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 03:40:59 +0500

    Initial Commit

---
+++ Yan_SwiftUI_vneschnij_vid_View/Yan_SwiftUI_vneschnij_vid_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_vneschnij_vid_ViewApp.swift
+//  Yan_SwiftUI_vneschnij_vid_View
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_vneschnij_vid_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
